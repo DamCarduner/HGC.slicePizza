d29249f46b0792db7e5fa32cad78f4387db311f2
damien <cardunerdamien@gmail.com>
2017-02-18 14:20:26 +0100

    first commit

---
+++ perso.googleHashCode.slicePizza/outputObject.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace perso.googleHashCode.slicePizza
+{
+    public class outputObject
+    {
+        public int nbSlices;
+        public List<int[]> slices;
+
+        public outputObject()
+        {
+            nbSlices = 0;
+            slices = new List<int[]>();
+        }
+
+        public void write(StreamWriter file)
+        {
+            file.WriteLine(nbSlices);
+            foreach (int[] slice in slices)
+            {
+                file.WriteLine(string.Format("{0} {1} {2} {3}", slice[0], slice[1], slice[2], slice[3]));
+            }
+        }
+    }
+}
---
+++ perso.googleHashCode.slicePizza/slicePizza.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace perso.googleHashCode.slicePizza
+{
+    public class slicePizza
+    {
+        public static string path = @"C:\Users\CARDUNER\Desktop\hashcode\";
+
+        public static string dataPath = path + @"data\";
+        public static string[] inFiles = Directory.GetFiles(dataPath);
+
+        public static string outPath = path + @"out\";
+
+        public static void Main(string[] args)
+        {
+            foreach (string file in inFiles)
+            {
+                string fileOutName = file.Split('\\').Last().Split('.').First() + ".out";
+                inputObject inputObject = null;
+                outputObject outputObject = null;
+                // Read file an read and put them inside c# object
+                using(var streamReader = new StreamReader(@file)) 
+                {
+                    inputObject = new inputObject(streamReader);
+                }
+
+                outputObject = inputObject.executeAlgorithm();
+
+                // Write file from c# object
+                using (var streamWriter = new StreamWriter(outPath + fileOutName))
+                {
+                    outputObject.write(streamWriter);
+                }
+            }
+        }
+    }
+}
